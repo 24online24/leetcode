1dac2c6f3dd1ca8cb6c4bd500d0254b3d664c98f
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-05-16 11:41:39 +0300

    Add Go, Rust and C# solutions

---
+++ 2331/csharp/Solution.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+  public bool EvaluateTree(TreeNode root)
+  {
+    return root.val switch
+    {
+      0 => false,
+      1 => true,
+      2 => EvaluateTree(root.left) || EvaluateTree(root.right),
+      3 => EvaluateTree(root.left) && EvaluateTree(root.right),
+      _ => false
+    };
+  }
+}
+
+public class TreeNode
+{
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+  public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+  {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
