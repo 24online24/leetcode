2ed498936b79ac4b335e1ba9836530363317be8c
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2025-09-14 18:09:33 +0300

    Optimize the fuck out of 966

--- 966/rust/src/lib.rs
+++ 966/rust/src/lib.rs
@@ -1,54 +1,37 @@
+use std::collections::{HashMap, HashSet};
+
 impl Solution {
     pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
-        #[derive(PartialEq, Eq, PartialOrd, Ord)]
-        enum MatchType {
-            None,
-            Vowel,
-            Case,
+        fn unvowel(word: &str) -> String {
+            word.to_ascii_lowercase()
+                .chars()
+                .map(|chr| if "aeiou".contains(chr) { '_' } else { chr })
+                .collect()
         }
 
-        let vowels: Vec<u8> = "aeiou".into();
+        let mut exact_words: HashSet<String> = HashSet::new();
+        let mut lower_case_words: HashMap<String, &String> = HashMap::new();
+        let mut unvoweled_words: HashMap<String, &String> = HashMap::new();
+        wordlist.iter().for_each(|word| {
+            exact_words.insert(word.clone());
+            lower_case_words
+                .entry(word.to_ascii_lowercase())
+                .or_insert(word);
+            unvoweled_words.entry(unvowel(word)).or_insert(word);
+        });
 
         queries
             .into_iter()
             .map(|query| {
-                let mut best_match = "".to_owned();
-                let mut best_match_type = MatchType::None;
-                let query_len = query.len();
-                for word in &wordlist {
-                    if query_len != word.len() {
-                        continue;
-                    }
-                    if query == *word {
-                        best_match = word.clone();
-                        break;
-                    } else {
-                        let query_lowercase = query.to_ascii_lowercase();
-                        let word_lowercase = word.to_ascii_lowercase();
-                        if best_match_type < MatchType::Case && query_lowercase == *word_lowercase {
-                            best_match = word.clone();
-                            best_match_type = MatchType::Case;
-                        } else if best_match_type == MatchType::None {
-                            let query_bytes = query_lowercase.as_bytes();
-                            let word_bytes = word_lowercase.as_bytes();
-                            let mut i = 0;
-                            while i < query_len {
-                                if query_bytes[i] != word_bytes[i]
-                                    && (!vowels.contains(&query_bytes[i])
-                                        || !vowels.contains(&word_bytes[i]))
-                                {
-                                    break;
-                                }
-                                i += 1;
-                            }
-                            if i == query_len {
-                                best_match = word.clone();
-                                best_match_type = MatchType::Vowel;
-                            }
-                        }
-                    }
+                if exact_words.contains(&query) {
+                    query
+                } else if let Some(&word) = lower_case_words.get(&query.to_ascii_lowercase()) {
+                    word.clone()
+                } else if let Some(&word) = unvoweled_words.get(&unvowel(&query)) {
+                    word.clone()
+                } else {
+                    "".to_string()
                 }
-                best_match
             })
             .collect()
     }
@@ -86,12 +69,7 @@ mod tests {
         assert_eq!(
             vec!["KiTe"],
             Solution::spellchecker(
-                vec![
-                    "KiTe".to_string(),
-                    "kite".to_string(),
-                    "hare".to_string(),
-                    "Hare".to_string()
-                ],
+                vec!["KiTe".to_string(), "kite".to_string(),],
                 vec!["Kite".to_string()]
             )
         );
