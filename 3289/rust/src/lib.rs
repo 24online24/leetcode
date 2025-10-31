86e76906b245c044dd694a70ded8b632c2e7d1bc
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2025-10-31 12:36:54 +0200

    Make 3289 *weird*

--- 3289/rust/src/lib.rs
+++ 3289/rust/src/lib.rs
@@ -2,14 +2,10 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = Vec::with_capacity(2);
         let mut appeared = HashSet::new();
-        for num in nums {
-            if !appeared.insert(num) {
-                result.push(num);
-            }
-        }
-        result
+        nums.into_iter()
+            .filter(|num| !appeared.insert(*num))
+            .collect()
     }
 }
 
